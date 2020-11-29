9a0cc44ddf141431d804af88dd0cbceccdb666bf
Joel A. Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2020-11-29 13:06:23 -0300

    exposed WLS for testing

--- js/cav.js
+++ js/cav.js
@@ -1,8 +1,11 @@
 import App from "./engine/app.js";
+import WaterService from "./services/water-service.js";
 
 const app = new App({
   components: ["cav-page"],
 });
 
-app.onReady(() => {});
+app.onReady(() => {
+  window.waterLog = new WaterService();
+});
 app.initialize();
